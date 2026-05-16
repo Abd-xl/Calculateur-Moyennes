824ab03b148c5c4ced8f49dc5585caa485782963
Abd-xl <Abdxcl225@protonmail.com>
2026-05-16 15:55:57 +0000

    nettoyage code, ajout de la possibilité de voir les notes enregistrés

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import json
 
-print('Welcome to the note calculator')
+#--- Variables ----
+notes = {}
+coef = {}
+
+#--- Fonctions ----
 
 def calculer_moyenne(liste_notes):
     return sum(liste_notes) / len(liste_notes)
-
-notes = {}
-coef = {}
 
 def sauvegarder():
     with open('notes.json' , 'w') as f:
@@ -22,9 +23,11 @@ def charger():
     except FileNotFoundError:
         pass
 
+# --- Menus ----
+
 def ajouter_notes():
     while True:
-        matiere = input('Matiere (ou fin pour arreter) ')
+        matiere = input('\nMatiere (ou fin pour terminer) :')
         if matiere.lower() == 'fin':
             break
         if matiere not in notes:
@@ -34,60 +37,83 @@ def ajouter_notes():
                    coef[matiere] = int(input(f'Coefficient {matiere} :'))
                    break
                except ValueError:
-                print('Entrez un nombre entier.')
+                print('Veuillez entrez un nombre entier.')
 
         while True:
             try:
                 nb = int(input(f'Combien de notes en {matiere} ?'))
                 break
             except ValueError:
-                print('Choisissez un numero')
+                print('Veuillez entrez un nombre entier.')
+
         for i in range(nb):
             while True:
                 try:
-                    note = float(input(f'Note {i + 1} :'))
+                    note = float(input(f'   Note {i + 1} : '))
                     if 0 <= note <= 20:
                         notes[matiere].append(note)
                         break
                     else:
-                        print('Note invalide')
+                        print('Hum please mets ta vrai note !')
                 except ValueError:
-                    print('Choisissez un numero')
-        sauvegarder()
+                    print('Veuillez entrez un nombre valide.')
 
+        sauvegarder()
+        print(f'Ok Notes de {matiere} enregistrées.')
 
 def voir_moyenne():
     if not notes:
-        print('Aucune note enregistrée')
+        print('Aucune note enregistrée.')
         return
+
+    print('\n--- Moyennes par matiere ---')
     somme_ponderee = 0
     somme_coef = 0
+
     for matiere in notes:
         moy = calculer_moyenne(notes[matiere])
-        print(f'{matiere} : {moy:.2f}')
-
+        print(f'  {matiere} (coeff. {coef[matiere]}) : {moy:.2f}/20')
         somme_ponderee += moy*coef[matiere]
         somme_coef += coef[matiere]
-    print(f'Moyenne Generale : {somme_ponderee /somme_coef:.2f}')
+
+    print(f'\n  Moyenne Generale : {somme_ponderee /somme_coef:.2f}/20')
+
+def voir_notes():
+    if not notes:
+        print('Aucune note enregistrée. ')
+        return
+
+    print('\n--- Notes par matiere ---')
+    for matiere in notes:
+        print(f'\n  {matiere} : ')
+        for note in notes[matiere]:
+            print(f'  {note}/20')
+
+# --- Demarage ----
 
 charger()
+print('---------Welcome to the note calculator------------')
 
 while True:
     print('\n--- MENU ---')
-    print('1. ajouter des notes')
+    print('1. Ajouter des notes')
     print('2. Voir les moyennes')
-    print('3. Quiter')
-    choix = input('Choisissez un choix : ')
+    print('3. Voir les notes')
+    print('4. Quitter')
+
+    choix = input('\nVotre choix ? : ')
 
     if choix == '1':
         ajouter_notes()
     elif choix == '2':
         voir_moyenne()
     elif choix == '3':
-        print('BYE')
+        voir_notes()
+    elif choix == '4':
+        print('See you soon!')
         break
     else:
-        print('Choisissez un choix : ')
+        print('Choix invalide')
 
 
 
